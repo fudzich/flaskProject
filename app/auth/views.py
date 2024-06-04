2bb34428c44fa4ce510ad4cc073fdea5d55fde3e
fudzich <vsevolod656@gmail.com>
2024-06-04 19:24:26 +0300

    lab 9

--- app/auth/views.py
+++ app/auth/views.py
@@ -7,6 +7,10 @@ from .forms import LoginForm, RegistrationForm
 from .. import db
 from ..models import User, Gender
 
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
+import webbrowser
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -25,15 +29,33 @@ def login():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(username=form.username.data,
-#                    password=form.password.data,
-                    gender=Gender.query.get(form.gender.data))
-        db.session.add(user)
-        user.set_password(form.password.data)
-        db.session.commit()
-        return redirect(url_for('auth.login'))
+        webbrowser.open_new_tab(url_for('auth.google_auth'))
+        if google_auth():
+            user = User(username=form.username.data,
+#                       password=form.password.data,
+                        gender=Gender.query.get(form.gender.data))
+            db.session.add(user)
+            user.set_password(form.password.data)
+            db.session.commit()
+            return redirect(url_for('auth.login'))
+        else:
+            return render_template('regTemplate.html', form=form)
     print(form.errors)
     return render_template('regTemplate.html', form=form)
+
+
+@auth.route('/oauth')
+def google_auth():
+    flow = (InstalledAppFlow.from_client_secrets_file(
+        'client_secret.json',
+        scopes=['openid', 'https://www.googleapis.com/auth/userinfo.email',
+                'https://www.googleapis.com/auth/userinfo.profile']))
+    flow.run_local_server()
+    credentials = flow.credentials
+    user_info_service = build('oauth2', 'v2', credentials=credentials)
+    user_info = user_info_service.userinfo().get().execute()
+
+    return user_info['verified_email']
 
 
 @auth.route('/logout')
